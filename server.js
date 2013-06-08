ef6f6cf91fca0d62e571d9a327ec3b700c36ba90
Jofell Gallardo <jofell@qryo.it>
2013-06-08 20:09:06 +0800

    Finished slide converter in preparation for Jesse's porting to ActiveGate

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express')
   , stylus = require('stylus')
+  , im = require('imagemagick')
+  , mkpath = require('mkpath')
   , nib = require('nib');
+  
 
 // Adding NIB local routine
 var app = express();
@@ -23,12 +26,46 @@ app.use(
     }
   )
 );
+app.use(express.bodyParser({ keepExtensions: true, uploadDir: "uploads" })); 
 
 app.get('/', function (req, res) {
   res.render('index',
     { title : 'Home' }
     )
 });
+
+app.post('/grab', function (req, res) {
+  // connect-form adds the req.form object
+  // we can (optionally) define onComplete, passing
+  // the exception (if any) fields parsed, and files parsed
+  im.identify(['-format', '%n', req.files.file.path], function(err, pages){
+    if (err) throw err;
+    
+    var iter = 0;
+    var currTime = new Date().getTime() + '';
+    var outFolder = __dirname + '/static/images/slides/' + currTime;
+    
+    mkpath( outFolder, function (err) {
+      if (err) throw err;
+      console.log('Directory structure red/green/violet created');
+    });
+    
+    var seriesConvert = function(iteration) {
+      
+      im.convert(['-density', 288, req.files.file.path +'[' + iteration + ']', 
+                  '-resize', '25%', outFolder + '/slide-' + iteration + '.jpg'], 
+       function(err, stdout){
+         if (err) throw err;
+         if (iter == 0) res.end("upload complete");
+         iter++;
+         if (iter < pages) seriesConvert(iter);
+       });
+    }
+    
+    seriesConvert(iter);
+  });
+});
+
 
 app.use(express.static(__dirname + '/public'));
 app.use('/static', express.static(__dirname + '/static'));
