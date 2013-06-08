d20a2a11cd528bc9c27629d977ec1851b1f08e49
Jofell Gallardo <jofell@qryo.it>
2013-06-08 15:15:56 +0800

    Adding stuff to test the installed node.js modules

--- server.js
+++ server.js
@@ -1,6 +1,35 @@
-var express = require('express');
+var express = require('express')
+  , stylus = require('stylus')
+  , nib = require('nib');
+
+// Adding NIB local routine
 var app = express();
 
+function compile(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .use(nib())
+}
+
+// Set the views folder
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.logger('dev'));
+app.use(
+  stylus.middleware({ 
+    src: __dirname + '/public', 
+    compile: compile
+    }
+  )
+);
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+  res.render('index',
+    { title : 'Home' }
+    )
+});
 
 app.listen(3000);
 console.log('Listening on port 3000');
